a17293852daecf47ef057ee9dc59836d42fd88b0
Aaron <7063649+ahcode0919@users.noreply.github.com>
2020-10-13 01:42:51 -0700

    Home page mit page tests (#4)
    * Add Base Page
    
    * Add test setup
    
    * Homepage and MIT License page tests

---
+++ tests/test_home_page.py
@@ -0,0 +1,13 @@
+from pages.home_page import HomePage
+from setup.setup import UITest
+
+
+class TestHomePage(UITest):
+    def test_page_load(self):
+        home_page = HomePage(self.driver).open()
+        assert home_page.is_page_loaded()
+
+    def test_mit_page_link(self):
+        home_page = HomePage(self.driver).open()
+        mit_license_page = home_page.open_mit_license_page()
+        assert mit_license_page.is_page_loaded()
